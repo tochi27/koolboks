e77f49811618d2c23e727727d05f7a1be678802f
Tochukwu Amaechina H <128059252+tochi27@users.noreply.github.com>
2023-09-08 14:48:52 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hi Koolboks. I'm here to clone your website, hopefully</h1>
+    </div>
+  )
+}
+
+export default App
